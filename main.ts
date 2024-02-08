63af36c7319fbaaa6cea37a2c40e8257f4598967
Arusa Hussain <124728332+hussainarusa@users.noreply.github.com>
2024-02-08 15:09:45 -0600

    transmitter

--- main.ts
+++ main.ts
@@ -1,22 +1,17 @@
-let count = 0
 finch.startFinch()
+let num = 1
+radio.setGroup(num)
+basic.showNumber(num)
 basic.forever(function () {
-    count = 0
-    for (let index = 0; index < 100; index++) {
-        finch.setTail(TailPort.All, count, 100 - count, 0)
-        count += 1
-        basic.pause(100)
-    }
-    count = 0
-    for (let index = 0; index < 100; index++) {
-        finch.setTail(TailPort.All, 100 - count, 0, count)
-        count += 1
-        basic.pause(100)
-    }
-    count = 0
-    for (let index = 0; index < 100; index++) {
-        finch.setTail(TailPort.All, 0, count, 100 - count)
-        count += 1
-        basic.pause(100)
+    if (input.buttonIsPressed(Button.AB)) {
+        radio.sendNumber(0)
+    } else if (input.buttonIsPressed(Button.A)) {
+        radio.sendNumber(1)
+    } else if (input.buttonIsPressed(Button.B)) {
+        radio.sendNumber(2)
+    } else if (input.rotation(Rotation.Pitch) > 100) {
+        radio.sendNumber(3)
+    } else {
+        radio.sendNumber(4)
     }
 })
